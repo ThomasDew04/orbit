8e72f291e6ee7abb1b9c5dac108342ad82c57976
ThomasDew04 <thomas1.dewilde@gmail.com>
2025-01-30 16:35:35 +0100

    update

--- src/components/Sun.jsx
+++ src/components/Sun.jsx
@@ -9,12 +9,12 @@ function Sun({ rotation }) {
       animate={{ rotateX: rotation.x, rotateY: rotation.y }}
       transition={{ type: "spring", stiffness: 100, damping: 25 }}
     >
-      <div className="sun-face front">Front</div>
-      <div className="sun-face back">Back</div>
-      <div className="sun-face left">Left</div>
-      <div className="sun-face right">Right</div>
-      <div className="sun-face top">Top</div>
-      <div className="sun-face bottom">Bottom</div>
+      <div className="sun-face front" />
+      <div className="sun-face back" />
+      <div className="sun-face left" />
+      <div className="sun-face right"/>
+      <div className="sun-face top" />
+      <div className="sun-face bottom" />
     </motion.div>
   );
 }
