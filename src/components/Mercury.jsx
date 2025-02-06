bafe3573b07a456e5bc5483e1944b071150da4db
ThomasDew04 <thomas1.dewilde@gmail.com>
2025-02-06 20:14:52 +0200

    Mercerius added

--- src/components/Mercury.jsx
+++ src/components/Mercury.jsx
@@ -1,9 +1,16 @@
 function Mercury() {
     return (
-        <div className="mercury-orbit" >
-            
+        <div className="mercury-orbit">
+            <div className="mercury">
+                <div className="mercury-face front" />
+                <div className="mercury-face back" />
+                <div className="mercury-face left" />
+                <div className="mercury-face right" />
+                <div className="mercury-face top" />
+                <div className="mercury-face bottom" />
+            </div>
         </div>
     );
-  }
-  
-  export default Mercury;
+}
+
+export default Mercury;
